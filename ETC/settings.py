f1c699e21f91c5111f921a2419a0733c0fa4d0a7
podestal <luisrodriguezpodesta@gmail.com>
2024-07-12 15:13:00 -0500

    allowed hosts

--- ETC/settings.py
+++ ETC/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['etc-1qajmpve7-luis-rodriguezs-projects-4856b3ea.vercel.app']
 
 
 # Application definition
