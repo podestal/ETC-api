16fee32e7456d2ab88b10dd91ffe194c26f3200d
podestal <luisrodriguezpodesta@gmail.com>
2024-07-12 15:30:39 -0500

    allow all hosts

--- ETC/settings.py
+++ ETC/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', '*']
 
 # Application definition
 
