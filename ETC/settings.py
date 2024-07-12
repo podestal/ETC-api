0301cb0b3844d5fb89949cf3430e614a81d9ffef
podestal <luisrodriguezpodesta@gmail.com>
2024-07-12 15:09:45 -0500

    posible fix to 500 error on vercel

--- ETC/settings.py
+++ ETC/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.verce.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
